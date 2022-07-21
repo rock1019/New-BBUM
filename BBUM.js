16d4a8bc967d68c7146081ef88706ec0777a9bcd
rock1019 <98945479+rock1019@users.noreply.github.com>
2022-07-21 16:01:26 +0900

    Update BBUM.js

--- BBUM.js
+++ BBUM.js
@@ -1,7 +1,9 @@
 console.log("making BBUM");
 
 let WELLCOME="BBUMHUB에 오신것을 환영합니다!";
+let 알림="사진 클릭시 이동"
 // 자바 스크립트에서 변수를 선언할때 
 //변할수있는 값은 let 변하지 않는값은 const를 사용
 
 alert(WELLCOME); //경고창
+alert(알림);
